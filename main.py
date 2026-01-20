148e2f33af6918be89f13253d64792d9cad19a0e
kovertonTHEdev <gizana99p@gmail.com>
2026-01-20 17:10:21 +0200

    commit 4

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 print ("Hello World!")
 print("Hello Git")
 
-
-name = input ( "What is your name? ")
-"Andrew" < True
-print ("Hello, Wendy")
-
-"Wendy" < False
-
 #код чтобы могли читать
+
+
+
+age = 18
+is_adult = age >= 18  # True
+
+age = 15
+is_adult = age >= 18  # False
