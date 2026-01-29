cb073da067bbf885e31e09a1ef590a0d838abd92
kovertonTHEdev <gizana99p@gmail.com>
2026-01-29 12:27:43 +0200

    commit27

--- #### TASK.py
+++ #### TASK.py
@@ -1,10 +1,16 @@
 num = int(input("Enter the integer (0 to 100): "))
 sum = 0
 b = 0
-
 while b < num:
-    sum = num + b + 1
-print(sum)
+    if num  <= 100:
+        sum = sum + b + 1
+        print(sum)
+
+    if num > 100:
+        print("Incorrect number")
+        break
+
+
 
 
     
