63e31c04a5fd6757268a3924ae66dc16d7a8b175
kovertonTHEdev <gizana99p@gmail.com>
2026-01-29 17:57:22 +0200

    commit28

--- ### TASK.py
+++ ### TASK.py
@@ -547,3 +547,10 @@ def discount_price(price: float, discount: float) -> float:
 
     apply_discount()
     return price
+
+### TASK
+def get_fullname(first_name, last_name, middle_name =""):
+    if middle_name:
+       return(f"{first_name} {middle_name} {last_name}")
+    else:
+       return(f"{first_name} {last_name}")   
--- #### TASK.py
+++ #### TASK.py
@@ -1,4 +1,7 @@
-
-        
-    
-        
+def format_string(string, length):
+    spaces = (length - len(string)) // 2
+    another_string = " " * spaces
+    if len(string) >= length:
+        return string
+    if len(string) < length:
+        return another_string + string     
