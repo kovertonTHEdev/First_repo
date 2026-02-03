70788dce3dbfb7cca22e5c701924b656264cd88f
kovertonTHEdev <gizana99p@gmail.com>
2026-02-03 18:01:36 +0200

    commit30

--- ### TASK.py
+++ ### TASK.py
@@ -1085,7 +1085,55 @@ print(b)  # Виведе 10000000
 c = 1_000_000_000
 print(c)  # Виведе 1000000000
 
+#########################################
 
+games = [
+    {"id": 101, "title": "Cyberpunk 2077"},
+    {"id": 102, "title": "The Witcher 3"},
+    {"id": 103, "title": "SWAT 4"},
+    {"id": 104, "title": "Metro 2033"},
+]
+
+owned_ids = {102, 103}
+installed_ids = {103}
+
+for game in games:
+    if game ["id"] in owned_ids:
+        status = "INSTALL"
+        if game ["id"] in installed_ids:
+                status = "PLAY"
+    else: 
+        status = "BUY"
+    print(f'{game["title"]}: {status}')
+
+#########################################
+
+catalog = {
+    101: "Cyberpunk 2077",
+    102: "The Witcher 3",
+    103: "SWAT 4",
+}
+
+cart_ids = [103, 999, 101, 103]
+
+for game_id in cart_ids:
+    if game_id in catalog:
+        print(f"OK: {catalog[game_id]}")
+    else:
+        print(f"MISSING: {game_id}")
+
+#########################################
+
+profile = {
+    "username": "andrewn",
+    "email": "andrew@gmail.com",
+    "age": 24,
+    "country": "Ukraine"
+}
+
+
+for key, value in profile.items():
+     print(f"{key} : {value}")
 
 
 
--- #### TASK.py
+++ #### TASK.py
@@ -1,3 +1,47 @@
-d = {"a": 1, "b": 2}
-for k, v in d.items():
-    print(k, v)
+games = [
+    {"id": 101, "title": "Cyberpunk 2077"},
+    {"id": 102, "title": "The Witcher 3"},
+    {"id": 103, "title": "SWAT 4"},
+    {"id": 104, "title": "Metro 2033"},
+]
+
+owned_ids = {102, 103}
+installed_ids = {103}
+
+for game in games:
+    if game ["id"] in owned_ids:
+        status = "INSTALL"
+        if game ["id"] in installed_ids:
+                status = "PLAY"
+    else: 
+        status = "BUY"
+    print(f'{game["title"]}: {status}')
+
+
+
+catalog = {
+    101: "Cyberpunk 2077",
+    102: "The Witcher 3",
+    103: "SWAT 4",
+}
+
+cart_ids = [103, 999, 101, 103]
+
+for game_id in cart_ids:
+    if game_id in catalog:
+        print(f"OK: {catalog[game_id]}")
+    else:
+        print(f"MISSING: {game_id}")
+
+
+
+profile = {
+    "username": "andrewn",
+    "email": "andrew@gmail.com",
+    "age": 24,
+    "country": "Ukraine"
+}
+
+
+for key, value in profile.items():
+     print(f"{key} : {value}")
