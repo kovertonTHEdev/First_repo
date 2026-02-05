af07185bade3eebb79371f45597057e9fdb31b1a
kovertonTHEdev <gizana99p@gmail.com>
2026-02-05 17:42:02 +0200

    commit31

--- #### TASK.py
+++ #### TASK.py
@@ -1,13 +1,20 @@
+
+
+########## ДЗ -1 
 from datetime import datetime
 
 date = "2020-10-09"
 def get_days_from_today(date):
-    pass
     try:
         formatted_date = datetime.strptime(date, "%Y-%m-%d") 
     except ValueError:
         return 
-    result = get_days_from_today("2020-10-09")
     current_date = datetime.today()
-    get_days_from_today = current_date.toordinal() - formatted_date.toordinal()
-    return print(result)
+    days_count = current_date.toordinal() - formatted_date.toordinal()
+    return days_count
+
+########## ДЗ -2
+get_numbers_ticket(min, max, quantity)
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
+
