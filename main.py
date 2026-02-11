beffed9a01d29d35dc7e1a4a8852e7eba294673f
kovertonTHEdev <gizana99p@gmail.com>
2026-02-11 18:35:42 +0200

    commit42

--- main.py
+++ main.py
@@ -1872,26 +1872,7 @@ from utility import nice_function, not_bad
 nice_function()
 not_bad("Test string")
 
-
-
-
-from joke import get_random_joke
-
-def main():
-    name = input("Будь ласка, введіть ваше ім'я: ")
-    print(f"Привіт, {name}!")
-
-    while True:
-        user_response = input(f"{name}, бажаєте почути анекдот? (так/ні): ").lower()
-        if user_response == "так":
-            print(get_random_joke())
-        elif user_response == "ні":
-            print(f"До побачення, {name}!")
-            break
-
-if __name__ == "__main__":
-    main()
-
-
-
+# ----------------------------
+# Тема 87: Пакетний менеджер pip та створення віртуального оточення
+# ----------------------------
 
