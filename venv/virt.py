c48c5189f3e74aa0d4a842380cb2c18fb364e77e
kovertonTHEdev <gizana99p@gmail.com>
2026-02-12 19:28:42 +0200

    commit47

--- venv/virt.py
+++ venv/virt.py
@@ -1,24 +1,32 @@
 import sys
-import os
 from pathlib import Path
+from colorama import init, Fore
+init(autoreset=True)
 
 if len(sys.argv) < 2:
     print ("python hw03.py <доступ_до_директорії>")
     sys.exit(1)
-if len(sys.argv) > 1:
-    second_el = sys.argv[1]
-    second_el_pathobj = Path(second_el)
-    if not second_el_pathobj.exists():
-        print("Цей путь не існує")
-        sys.exit(1)
-    if not second_el_pathobj.is_dir():
-        print("Цієї директорії не існує")
-        sys.exit(1) 
+second_el = sys.argv[1]
+second_el_pathobj = Path(second_el)
+if not second_el_pathobj.exists():
+    print("Це розташування не існує")
+    sys.exit(1)
+if not second_el_pathobj.is_dir():
+    print("Цієї директорії не існує")
+    sys.exit(1) 
+print(second_el_pathobj.resolve())
 
 def function(path: Path, depth: int = 0):
     indent = " " * 4 * depth
-    for file in path.iterdir():
-        print(f"{indent}{file.name}")
+    try:
+        elements = sorted(path.iterdir(), key=lambda p: p.name)
+    except PermissionError:
+        print(f"У вас не має доступу до {path.name}")
+        return
+    for file in elements:
         if file.is_dir():
+            print(Fore.BLUE + f"{indent}+ {file.name}/")
             function(file, depth + 1)
-    function(second_el_pathobj, 0)
+        else:
+            print(Fore.GREEN + f"{indent}- {file.name}")
+function(second_el_pathobj, 0)
