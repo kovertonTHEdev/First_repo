cf414e48a225a9f621ed89ceb26297355b5d2d2a
kovertonTHEdev <gizana99p@gmail.com>
2026-02-12 20:23:04 +0200

    commit49

--- pets/general_1.py
+++ pets/general_1.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from cats_func import get_cats_info
 BASE = Path(__file__).resolve().parent
-CATS_FILE = BASE / "cats_file.txt"
+cats_file = BASE / "cats_file.txt"
 
-cats_info = get_cats_info(CATS_FILE)
+cats_info = get_cats_info(cats_file)
 print(cats_info)
