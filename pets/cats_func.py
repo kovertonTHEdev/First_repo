3d32b0ce08e55c7ca4682171c0fa4e71689f4a77
kovertonTHEdev <gizana99p@gmail.com>
2026-02-12 15:24:34 +0200

    commit46

--- pets/cats_func.py
+++ pets/cats_func.py
@@ -1,2 +1,20 @@
 def get_cats_info(CATS_FILE):
-    with open (CATS_FILE, "r", encoding="UTF-8"):
+    with open (CATS_FILE, "r", encoding="UTF-8") as file:
+        cats_list = []
+        for line in file:
+            cleaned_line = line.strip()
+            if not cleaned_line:
+                continue
+            parts = cleaned_line.split(",")
+            if len(parts) != 3:
+                continue
+            cat_id = parts[0]
+            name = parts[1]
+            age_text = parts[2]
+            try:
+                age = int(age_text)
+                cats_dict = {"id": cat_id, "name": name, "age": age}
+                cats_list.append(cats_dict)
+            except ValueError:
+                continue
+    return cats_list
--- pets/general_1.py
+++ pets/general_1.py
@@ -1,3 +1,7 @@
 from pathlib import Path
+from cats_func import get_cats_info
 BASE = Path(__file__).resolve().parent
 CATS_FILE = BASE / "cats_file.txt"
+
+cats_info = get_cats_info(CATS_FILE)
+print(cats_info)
