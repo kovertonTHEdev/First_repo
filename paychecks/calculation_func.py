30aba80ddc889834cb2a025f1b31862f7ce6fc5c
kovertonTHEdev <gizana99p@gmail.com>
2026-02-12 14:37:03 +0200

    commit45

--- paychecks/calculation_func.py
+++ paychecks/calculation_func.py
@@ -1,24 +1,19 @@
-import os
-def calculations (SALARIES_FILE): 
+def total_salary (SALARIES_FILE): 
         with open(SALARIES_FILE, 'r', encoding='utf-8') as file: #відкриття файлу
             total = 0 # акумулятор
             count = 0 # акумулятор
             for line in file:
                 cleaned_line = line.strip() # прибирає пробіли 
                 parts = cleaned_line.split(",") # робить список імен + ЗП
-                if len(parts) == 2:
-                    first_name = parts[0] 
+                if len(parts) == 2: 
                     salary_text = parts[1]
                     try:
                          salary = int(salary_text)
-                         total = total + salary
-                         count+=1
+                         total = total + salary # накопичення сум зарплат
+                         count+=1 #накопичення акум
                     except ValueError:
                          continue
                     
         if count > 0:
-             average = total / count 
-             return average 
-        elif count == 0:
-            average = 0
+             average = total / count # cереднє вираховуємо
         return (total, average)
--- paychecks/general.py
+++ paychecks/general.py
@@ -1,3 +1,7 @@
+from calculation_func import total_salary
 from pathlib import Path
 BASE = Path(__file__).resolve().parent
-SALARIES_FILE = BASE / "paychecks" / "salaries.txt"
+SALARIES_FILE = BASE / "salaries.txt"
+
+total, average = total_salary(SALARIES_FILE)
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
