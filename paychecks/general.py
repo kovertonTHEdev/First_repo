be49a400b03f1aac9c09ad077ef3be8e66c98c19
kovertonTHEdev <gizana99p@gmail.com>
2026-02-12 20:11:46 +0200

    commit49

--- paychecks/general.py
+++ paychecks/general.py
@@ -1,7 +1,7 @@
 from calculation_func import total_salary
 from pathlib import Path
 BASE = Path(__file__).resolve().parent
-SALARIES_FILE = BASE / "salaries.txt"
+salaries_file = BASE / "salaries.txt"
 
-total, average = total_salary(SALARIES_FILE)
+total, average = total_salary(salaries_file)
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
